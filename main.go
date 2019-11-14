2ad044a92bf46045695c2a035878a7f94abd5d05
Ricky Hsu <rickyhsu@qnap.com>
2019-11-14 17:34:13 +0800

    Add graceful shutdown

--- main.go
+++ main.go
@@ -4,13 +4,19 @@ import (
 	"Golang_Restful_API/pkg/models"
 	"Golang_Restful_API/pkg/routers"
 	"Golang_Restful_API/pkg/setting"
+	"context"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
 )
 
 func main() {
 	setting.Setup()
 	models.Setup()
+
 	router := routers.InitRouter()
 
 	s := &http.Server{
@@ -20,5 +26,25 @@ func main() {
 		WriteTimeout:   setting.ServerSetting.WriteTimeout,
 		MaxHeaderBytes: 1 << 20,
 	}
-	s.ListenAndServe()
+	//s.ListenAndServe()
+
+	go func() {
+		if err := s.ListenAndServe(); err != nil {
+			log.Printf("Listen: %s\n", err)
+		}
+	}()
+
+	quit := make(chan os.Signal)
+	signal.Notify(quit, os.Interrupt)
+	<- quit
+
+	log.Println("Shutdown Server ...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5 * time.Second)
+	defer cancel()
+	if err := s.Shutdown(ctx); err != nil {
+		log.Fatal("Server Shutdown:", err)
+	}
+
+	log.Println("Server exiting")
 }
